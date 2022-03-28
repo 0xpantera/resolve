9924b1be2a67e2e96c3214a9e356ffa3a5b59ccc
Francososa <franky.sosa@gmail.com>
2022-03-28 12:20:35 -0600

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use std::net::{SocketAddr, UdpSocket};
+use std::time::Duration;
+
+use clap::{App, Arg};
+use rand;
+use trust_dns::op::{Message, MessageType, OpCode, Query};
+use trust_dns::rr::domain::Name;
+use trust_dns::rr::record_type::RecordType;
+use trust_dns::serialize::binary::*;
+
+
+fn main() {
+    let app = App::new("resolve")
+        .about("A simple to use DNS resolver");
+}
